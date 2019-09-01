ee727e6001cf11515de05623284d2dfd67ad2a12
Christopher O'Bannon <Crobwan@gmail.com>
2019-09-01 14:28:46 -0500

    movies now append to the dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import {connect} from 'react-redux';
 
 
+
 class App extends Component {
+
+  componentDidMount=() => {
+this.props.dispatch({
+  type: 'GET_MOVIES'
+})
+  }
   // Renders the entire app on the DOM
   render() {
     return (
       <div className="App">
-
+<ul>
+  {this.props.reduxStore.movies.map(film => 
+   <li key = {film.id}> <img src={film.poster}/>{film.title} {film.description}   </li>
+  )}
+</ul>
         <p>Empty Page</p>
       </div>
     );
